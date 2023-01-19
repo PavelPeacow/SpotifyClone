b9219eacc5bd724dc95eedc21566eca0b8173a45
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2023-01-19 08:14:42 +0500

    initial commit

---
+++ SpotifyClone/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  SpotifyClone
+//
+//  Created by Павел Кай on 19.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+
+}
+
---
+++ SpotifyClone/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Павел Кай on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
