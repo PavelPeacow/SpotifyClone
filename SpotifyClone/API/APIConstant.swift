3aeaa9e1f5f4325f7df8d0614bf7d5885e70631d
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2023-02-02 22:15:30 +0500

    remove secrets

--- SpotifyClone/API/APIConstant.swift
+++ SpotifyClone/API/APIConstant.swift
@@ -6,8 +6,8 @@
 //
 
 final class APIConstant {
-    static let clientID = "8b74b4bf9f8c4e5098c21303c51bc747"
-    static let clientSecret = "62e673ac781c42e282e3ee68f731f0ff"
+    static let clientID = "YOUR_CLIENT_ID"
+    static let clientSecret = "YOUR_CLIENT_SECRET"
     static let callbackScheme =  "spotify-clone-login"
     
     static let redirect_uri = "spotify-clone-login://callback"
